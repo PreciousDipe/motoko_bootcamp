c325cb8f81af89c8819a01b94be498ae39451dd8
PreciousDipe <preciousdipe@gmail.com>
2023-01-10 21:48:20 +0100

    First commit

---
+++ src/FIRST_PROJECT_frontend/src/index.js
@@ -0,0 +1,19 @@
+import { FIRST_PROJECT_backend } from "../../declarations/FIRST_PROJECT_backend";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await FIRST_PROJECT_backend.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
